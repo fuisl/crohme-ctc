6c125ad1556b7b9bd1be1d42c15e8b78290d12c5
fuisl <135719107+fuisl@users.noreply.github.com>
2024-06-11 17:43:11 +0700

    feat(dataset): initialize dataset base

---
+++ data.py
@@ -0,0 +1,265 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+import pytorch_lightning as pl
+from torch.nn.utils.rnn import pad_sequence
+
+import glob
+import os
+
+import numpy as np
+import xml.etree.ElementTree as ET
+
+
+class Segment(object):
+    """Class to reprsent a Segment compound of strokes (id) with an id and label."""
+
+    __slots__ = ("id", "label", "strId")
+
+    def __init__(self, *args):
+        if len(args) == 3:
+            self.id = args[0]
+            self.label = args[1]
+            self.strId = args[2]
+        else:
+            self.id = "none"
+            self.label = ""
+            self.strId = set([])
+
+
+class Inkml(object):
+    """Class to represent an INKML file with strokes, segmentation and labels"""
+
+    __slots__ = ("fileName", "strokes", "strkOrder", "segments", "truth", "UI")
+
+    NS = {
+        "ns": "http://www.w3.org/2003/InkML",
+        "xml": "http://www.w3.org/XML/1998/namespace",
+    }
+
+    def __init__(self, *args):
+        self.fileName = None
+        self.strokes = {}
+        self.strkOrder = []
+        self.segments = {}
+        self.truth = ""
+        self.UI = ""
+        if len(args) == 1:
+            self.fileName = args[0]
+            self.loadFromFile()
+
+    def fixNS(self, ns, att):
+        """Build the right tag or element name with namespace"""
+        return "{" + Inkml.NS[ns] + "}" + att
+
+    def loadFromFile(self):
+        """load the ink from an inkml file (strokes, segments, labels)"""
+        tree = ET.parse(self.fileName)
+        # # ET.register_namespace();
+        root = tree.getroot()
+        for info in root.findall("ns:annotation", namespaces=Inkml.NS):
+            if "type" in info.attrib:
+                if info.attrib["type"] == "truth":
+                    self.truth = info.text.strip()
+                if info.attrib["type"] == "UI":
+                    self.UI = info.text.strip()
+        for strk in root.findall("ns:trace", namespaces=Inkml.NS):
+            self.strokes[strk.attrib["id"]] = strk.text.strip()
+            self.strkOrder.append(strk.attrib["id"])
+        segments = root.find("ns:traceGroup", namespaces=Inkml.NS)
+        if segments is None or len(segments) == 0:
+            return
+        for seg in segments.iterfind("ns:traceGroup", namespaces=Inkml.NS):
+            id = seg.attrib[self.fixNS("xml", "id")]
+            label = seg.find("ns:annotation", namespaces=Inkml.NS).text
+            strkList = set([])
+            for t in seg.findall("ns:traceView", namespaces=Inkml.NS):
+                strkList.add(t.attrib["traceDataRef"])
+            self.segments[id] = Segment(id, label, strkList)
+
+    def getTraces(self, height=256):
+        traces_array = [
+            np.array(
+                [p.strip().split() for p in self.strokes[id].split(",")], dtype="float"
+            )
+            for id in self.strkOrder
+        ]
+
+        ratio = height / (
+            (
+                np.concatenate(traces_array, 0).max(0)
+                - np.concatenate(traces_array, 0).min(0)
+            )[1]
+            + 1e-6
+        )
+        return [(trace * ratio).astype(int).tolist() for trace in traces_array]
+
+
+"""
+TODO: 
+- [ ] build vocabulary
+- [ ] remove duplicate (next in sequence)
+- [ ] normalize (delta x, delta y)
+- [ ] concat traces in a single array (n, 2)
+- [ ] pad to max length (collate)
+"""
+
+
+class InkmlDataset(Dataset):
+    def __init__(self, annotation, root_dir="dataset"):
+        with open(annotation, "r") as f:
+            self.files = f.readlines()
+
+        self.vocab = {
+            "": 0,
+            "-": 1,
+            "\\times": 2,
+            "\\{": 3,
+            "\\beta": 4,
+            "m": 5,
+            "Above": 6,
+            "E": 7,
+            "\\infty": 8,
+            "\\forall": 9,
+            "\\cos": 10,
+            "8": 11,
+            ")": 12,
+            "/": 13,
+            "\\sum": 14,
+            "n": 15,
+            "\\pi": 16,
+            "\\geq": 17,
+            "C": 18,
+            "a": 19,
+            "\\mu": 20,
+            "S": 21,
+            "]": 22,
+            "R": 23,
+            "\\gt": 24,
+            "Sup": 25,
+            "x": 26,
+            "p": 27,
+            "\\ldots": 28,
+            "\\int": 29,
+            "\\sqrt": 30,
+            "f": 31,
+            "Right": 32,
+            "k": 33,
+            "\\log": 34,
+            "\\leq": 35,
+            "j": 36,
+            "w": 37,
+            "7": 38,
+            "y": 39,
+            "\\exists": 40,
+            "d": 41,
+            "[": 42,
+            "q": 43,
+            "\\div": 44,
+            "NoRel": 45,
+            "\\phi": 46,
+            "1": 47,
+            "g": 48,
+            "X": 49,
+            "\\in": 50,
+            "\\gamma": 51,
+            "\\prime": 52,
+            "4": 53,
+            "\\pm": 54,
+            "T": 55,
+            "F": 56,
+            "N": 57,
+            "\\lt": 58,
+            "o": 59,
+            "u": 60,
+            "h": 61,
+            "s": 62,
+            "6": 63,
+            "c": 64,
+            "(": 65,
+            "A": 66,
+            "!": 67,
+            "P": 68,
+            "L": 69,
+            "COMMA": 70,
+            "i": 71,
+            "b": 72,
+            "t": 73,
+            "+": 74,
+            "\\neq": 75,
+            "9": 76,
+            "3": 77,
+            "G": 78,
+            ".": 79,
+            "e": 80,
+            "M": 81,
+            "r": 82,
+            "\\sin": 83,
+            "\\lim": 84,
+            "\\lambda": 85,
+            "I": 86,
+            "\\rightarrow": 87,
+            "Inside": 88,
+            "\\sigma": 89,
+            "V": 90,
+            "\\theta": 91,
+            "l": 92,
+            "=": 93,
+            "\\tan": 94,
+            "z": 95,
+            "2": 96,
+            "H": 97,
+            "0": 98,
+            "5": 99,
+            "Below": 100,
+            "|": 101,
+            "\\Delta": 102,
+            "\\alpha": 103,
+            "B": 104,
+            "Y": 105,
+            "v": 106,
+            "\\}": 107,
+            "Sub": 108,
+        }
+
+        self.root_dir = root_dir
+        self.inks = []
+        self.labels = []
+
+        for line in self.files:
+            path, label = line.strip("\n").split("\t")
+            inkml = Inkml(os.path.join(self.root_dir, path))
+
+            self.inks.append(inkml)
+            self.labels.append(label)
+
+    def normalize(self):
+        for ink in self.inks:
+            traces = ink.getTraces()
+            for i in range(len(traces) - 1):
+                traces[i + 1] = np.array(traces[i + 1]) - np.array(traces[i])
+
+    # def concatTraces(self):
+    #     for ink in self.inks:
+    #         ink.traces = np.concatenate(ink.getTraces(), 0)
+
+    # def padTraces(self):
+    #     for ink in self.inks:
+    #         ink.traces = pad_sequence(
+    #             [torch.tensor(trace) for trace in ink.getTraces()], batch_first=True])
+            
+
+    def __len__(self):
+        return len(self.inks)
+
+    def __getitem__(self, idx):
+        return self.inks[idx].getTraces(), self.labels[idx]
+    
+
+"""
+input file anot root dir
+output delta_trace, label [0, 2 ,2 , 13]
+
+
+pen up pen down
+"""
