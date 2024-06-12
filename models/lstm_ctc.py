0f4b25980113ec9062254399e78938a717081542
fuisl <135719107+fuisl@users.noreply.github.com>
2024-06-12 21:54:58 +0700

    feat(model): implemented CTC Loss.

--- models/lstm_ctc.py
+++ models/lstm_ctc.py
@@ -24,7 +24,7 @@ class LSTM_TemporalClassification_PL(pl.LightningModule):
         self.model = LSTM_TemporalClassification(
             input_size, hidden_size, num_layers, num_classes
         )
-        self.criterion = nn.CTCLoss(blank=0, zero_infinity=True)
+        self.criterion = nn.CTCLoss(blank=0, zero_infinity=True, reduction="mean")
 
     def forward(self, x):
         return self.model(x)
@@ -35,7 +35,7 @@ class LSTM_TemporalClassification_PL(pl.LightningModule):
         y_hat = F.log_softmax(y_hat, dim=2)
         input_lengths = torch.full((y_hat.size(0),), y_hat.size(1), dtype=torch.long)
         target_lengths = torch.full((y_hat.size(0),), y.size(1), dtype=torch.long)
-        loss = self.criterion(y_hat, y, input_lengths, target_lengths)
+        loss = self.criterion(y_hat.permute(1, 0, 2), y, input_lengths, target_lengths)
         return loss
 
     def validation_step(self, batch, batch_idx):
@@ -44,7 +44,7 @@ class LSTM_TemporalClassification_PL(pl.LightningModule):
         y_hat = F.log_softmax(y_hat, dim=2)
         input_lengths = torch.full((y_hat.size(0),), y_hat.size(1), dtype=torch.long)
         target_lengths = torch.full((y_hat.size(0),), y.size(1), dtype=torch.long)
-        loss = self.criterion(y_hat, y, input_lengths, target_lengths)
+        loss = self.criterion(y_hat.permute(1, 0, 2), y, input_lengths, target_lengths)
         return loss
 
     def test_step(self, batch, batch_idx):
@@ -53,11 +53,22 @@ class LSTM_TemporalClassification_PL(pl.LightningModule):
         y_hat = F.log_softmax(y_hat, dim=2)
         input_lengths = torch.full((y_hat.size(0),), y_hat.size(1), dtype=torch.long)
         target_lengths = torch.full((y_hat.size(0),), y.size(1), dtype=torch.long)
-        loss = self.criterion(y_hat, y, input_lengths, target_lengths)
+        loss = self.criterion(y_hat.permute(1, 0, 2), y, input_lengths, target_lengths)
         return loss
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        # TODO: optimize with lr_scheduler
+        # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        #     optimizer, mode="min", factor=0.1, patience=10, verbose=True
+        # )
+        # return {
+        #     "optimizer": optimizer,
+        #     "lr_scheduler": {
+        #         "scheduler": scheduler,
+        #         "monitor": "val_loss",
+        #     },
+        # }
         return optimizer
 
 
@@ -66,4 +77,14 @@ if __name__ == "__main__":
     # feature has shape (1, 3)
     x = torch.randn((10, 1621, 3))
     out = model(x)
+
+    optim = torch.optim.Adam(model.parameters(), lr=1e-3)
+    loss = nn.CTCLoss(blank=0, zero_infinity=True, reduction="mean")
+    target = torch.randint(1, 109, (10, 100), dtype=torch.long)
+    input_lengths = torch.full((10,), 1621, dtype=torch.long)
+    target_lengths = torch.full((10,), 100, dtype=torch.long)
+    y_hat = F.log_softmax(out, dim=2)
+    loss = loss(y_hat, target, input_lengths, target_lengths)
+    loss.backward()
+    optim.step()
     print(out.shape)
