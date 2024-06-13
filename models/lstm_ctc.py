cbb8f75947414ee1f1d5589ce1dcf7991c1dc6d8
fuisl <135719107+fuisl@users.noreply.github.com>
2024-06-13 15:39:08 +0700

    fix!: bug CTCLoss

--- models/lstm_ctc.py
+++ models/lstm_ctc.py
@@ -10,21 +10,24 @@ class LSTM_TemporalClassification(nn.Module):
 
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size, num_classes)
+        self.log_softmax = nn.LogSoftmax(dim=2)
 
     def forward(self, x):
         x, _ = self.lstm(x)
         x = self.fc(x)
+        x = self.log_softmax(x)
+
         return x
 
 
 class LSTM_TemporalClassification_PL(pl.LightningModule):
-    def __init__(self, input_size=3, hidden_size=128, num_layers=1, num_classes=109):
+    def __init__(self, input_size=3, hidden_size=128, num_layers=1, num_classes=109, blank=0):
         super(LSTM_TemporalClassification_PL, self).__init__()
 
         self.model = LSTM_TemporalClassification(
             input_size, hidden_size, num_layers, num_classes
         )
-        self.criterion = nn.CTCLoss(blank=0, zero_infinity=True, reduction="mean")
+        self.criterion = nn.CTCLoss(blank=blank, zero_infinity=True, reduction="mean")
 
     def forward(self, x):
         return self.model(x)
@@ -32,10 +35,10 @@ class LSTM_TemporalClassification_PL(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self.model(x)
-        y_hat = F.log_softmax(y_hat, dim=2)
         input_lengths = torch.full((y_hat.size(0),), y_hat.size(1), dtype=torch.long)
         target_lengths = torch.full((y_hat.size(0),), y.size(1), dtype=torch.long)
         loss = self.criterion(y_hat.permute(1, 0, 2), y, input_lengths, target_lengths)
+        self.log("train_loss", loss, on_epoch=True, prog_bar=True, logger=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
@@ -45,6 +48,7 @@ class LSTM_TemporalClassification_PL(pl.LightningModule):
         input_lengths = torch.full((y_hat.size(0),), y_hat.size(1), dtype=torch.long)
         target_lengths = torch.full((y_hat.size(0),), y.size(1), dtype=torch.long)
         loss = self.criterion(y_hat.permute(1, 0, 2), y, input_lengths, target_lengths)
+        self.log("val_loss", loss, on_epoch=True, prog_bar=True, logger=True)
         return loss
 
     def test_step(self, batch, batch_idx):
@@ -54,21 +58,22 @@ class LSTM_TemporalClassification_PL(pl.LightningModule):
         input_lengths = torch.full((y_hat.size(0),), y_hat.size(1), dtype=torch.long)
         target_lengths = torch.full((y_hat.size(0),), y.size(1), dtype=torch.long)
         loss = self.criterion(y_hat.permute(1, 0, 2), y, input_lengths, target_lengths)
+        self.log("test_loss", loss, on_epoch=True, prog_bar=True, logger=True)
         return loss
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
         # TODO: optimize with lr_scheduler
-        # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-        #     optimizer, mode="min", factor=0.1, patience=10, verbose=True
-        # )
-        # return {
-        #     "optimizer": optimizer,
-        #     "lr_scheduler": {
-        #         "scheduler": scheduler,
-        #         "monitor": "val_loss",
-        #     },
-        # }
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer, mode="min", factor=0.1, patience=10, verbose=True
+        )
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": {
+                "scheduler": scheduler,
+                "monitor": "val_loss",
+            },
+        }
         return optimizer
 
 
