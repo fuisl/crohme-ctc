96014e5c2f89cb42f2982837fc54aeaf1e6a3815
fuisl <135719107+fuisl@users.noreply.github.com>
2024-07-04 17:46:53 +0700

    feat: implement RelativePositionLoss

--- models/lstm_ctc.py
+++ models/lstm_ctc.py
@@ -5,6 +5,10 @@ import pytorch_lightning as pl
 from torchaudio.models.decoder import ctc_decoder, cuda_ctc_decoder
 import numpy as np
 from .utils.edit import TokenEditDistance
+from .utils.loss import RelativePositionLoss
+
+# Constants
+ALPHA = 0.05  # Relative Position Loss weight
 
 
 class LSTM_TemporalClassification(nn.Module):
@@ -56,6 +60,7 @@ class LSTM_TemporalClassification_PL(pl.LightningModule):
             bidirectional=bidirectional,
         )
         self.criterion = nn.CTCLoss(blank=blank, zero_infinity=True, reduction="mean")
+        self.relation_criterion = RelativePositionLoss()
         self.vocab = {
             "": 0,
             "-": 1,
@@ -171,6 +176,7 @@ class LSTM_TemporalClassification_PL(pl.LightningModule):
         self.relation_idx = [6, 100, 88, 45, 32, 108, 25]
         self.decoder = cuda_ctc_decoder(tokens=list(self.vocab.keys()))
         self.metric = TokenEditDistance()
+
     def forward(self, x):
         return self.model(x)
 
@@ -178,12 +184,18 @@ class LSTM_TemporalClassification_PL(pl.LightningModule):
         x, y, in_len, target_len = batch
         batch_size = x.size(0)
         y_hat = self.model(x)
-        input_lengths = torch.tensor(in_len)
-        target_lengths = torch.tensor(target_len)
-        loss = self.criterion(y_hat.permute(1, 0, 2), y, input_lengths, target_lengths)
-        # TODO: add additional loss
-        self.log("train_loss", loss, on_epoch=True, prog_bar=True, logger=True, batch_size=batch_size)
-        return loss
+        input_lengths = torch.tensor(in_len).cuda()
+        target_lengths = torch.tensor(target_len).cuda()
+
+        ctc_loss = self.criterion(y_hat.permute(1, 0, 2), y, input_lengths, target_lengths)
+        rel_loss = self.relation_criterion(y_hat, x[:, :, 3])  # pen_up
+
+        total_loss = ctc_loss + (ALPHA * rel_loss)
+
+        self.log("train_CTCloss", ctc_loss, on_epoch=True, prog_bar=True, logger=True, batch_size=batch_size)
+        self.log("train_RelLoss", rel_loss, on_epoch=True, prog_bar=True, logger=True, batch_size=batch_size)
+        self.log("train_loss", total_loss, on_epoch=True, prog_bar=True, logger=True, batch_size=batch_size)
+        return total_loss
 
     def validation_step(self, batch, batch_idx):
         x, y, in_len, target_len = batch
@@ -191,7 +203,11 @@ class LSTM_TemporalClassification_PL(pl.LightningModule):
         y_hat = self.model(x)
         input_lengths = torch.tensor(in_len).cuda()
         target_lengths = torch.tensor(target_len).cuda()
-        loss = self.criterion(y_hat.permute(1, 0, 2), y, input_lengths, target_lengths)
+
+        ctc_loss = self.criterion(y_hat.permute(1, 0, 2), y, input_lengths, target_lengths)
+        rel_loss = self.relation_criterion(y_hat, x[:, :, 3])  # pen_up
+
+        total_loss = ctc_loss + (ALPHA * rel_loss)
 
         keys = list(self.vocab.keys())
         decoded_output = self.decoder(y_hat, input_lengths.to(torch.int32))
@@ -210,13 +226,16 @@ class LSTM_TemporalClassification_PL(pl.LightningModule):
 
         wer = edit_distance / np.array([len(target.split()) for target in target_str_list]).mean()
 
-        self.log("val_loss", loss, on_epoch=True, prog_bar=True, logger=True, batch_size=batch_size)
+        self.log("val_CTCloss", ctc_loss, on_epoch=True, prog_bar=True, logger=True, batch_size=batch_size)
+        self.log("val_RelLoss", rel_loss, on_epoch=True, prog_bar=True, logger=True, batch_size=batch_size)
+        self.log("val_loss", total_loss, on_epoch=True, prog_bar=True, logger=True, batch_size=batch_size)
+
         self.log("edit_distance", edit_distance, on_epoch=True, prog_bar=True, logger=True, batch_size=batch_size)
-        self.log("relation_edit_distance", relation_edit_distance, on_epoch=True, prog_bar=True, logger=True, batch_size=batch_size)
-        self.log("symbol_edit_distance", symbol_edit_distance, on_epoch=True, prog_bar=True, logger=True, batch_size=batch_size)
+        self.log("relation_edit_distance", relation_edit_distance, on_epoch=True, prog_bar=False, logger=True, batch_size=batch_size)
+        self.log("symbol_edit_distance", symbol_edit_distance, on_epoch=True, prog_bar=False, logger=True, batch_size=batch_size)
         self.log("total_wer", wer, on_epoch=True, prog_bar=True, logger=True, batch_size=batch_size)
 
-        return loss
+        return total_loss
 
     def test_step(self, batch, batch_idx):
         x, y, in_len, target_len = batch
@@ -224,7 +243,11 @@ class LSTM_TemporalClassification_PL(pl.LightningModule):
         y_hat = self.model(x)
         input_lengths = torch.tensor(in_len).cuda()
         target_lengths = torch.tensor(target_len).cuda()
-        loss = self.criterion(y_hat.permute(1, 0, 2), y, input_lengths, target_lengths)
+
+        ctc_loss = self.criterion(y_hat.permute(1, 0, 2), y, input_lengths, target_lengths)
+        rel_loss = self.relation_criterion(y_hat, x[:, :, 3])
+
+        total_loss = ctc_loss + (ALPHA * rel_loss)
 
         keys = list(self.vocab.keys())
         decoded_output = self.decoder(y_hat, input_lengths.to(torch.int32))
@@ -243,13 +266,16 @@ class LSTM_TemporalClassification_PL(pl.LightningModule):
 
         wer = edit_distance / np.array([len(target.split()) for target in target_str_list]).mean()
 
-        self.log("val_loss", loss, on_epoch=True, prog_bar=True, logger=True, batch_size=batch_size)
+        self.log("test_CTCloss", ctc_loss, on_epoch=True, prog_bar=True, logger=True, batch_size=batch_size)
+        self.log("test_RelLoss", rel_loss, on_epoch=True, prog_bar=True, logger=True, batch_size=batch_size)
+        self.log("test_loss", total_loss, on_epoch=True, prog_bar=True, logger=True, batch_size=batch_size)
+
         self.log("edit_distance", edit_distance, on_epoch=True, prog_bar=True, logger=True, batch_size=batch_size)
         self.log("relation_edit_distance", relation_edit_distance, on_epoch=True, prog_bar=True, logger=True, batch_size=batch_size)
         self.log("symbol_edit_distance", symbol_edit_distance, on_epoch=True, prog_bar=True, logger=True, batch_size=batch_size)
         self.log("total_wer", wer, on_epoch=True, prog_bar=True, logger=True, batch_size=batch_size)
 
-        return loss
+        return total_loss
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
